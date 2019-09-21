9ee18908dc4fbfbdbd91fadf31f40c70401942a9
Oswaldo Rodriguez <Owner2303@gmail.com>
2019-09-21 11:24:26 -0400

    New Program!

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("================================================================================================");
+            //Variables Containing my Name and Location
+            string myName = "Oswaldo Rodriguez", myLocation = "Columbia, SC";
+            Console.WriteLine($"My name is {myName}.");
+            Console.WriteLine($"I currently live in {myLocation}.");
+            //Variable Containing the Date.
+            string todaysDate = DateTime.Today.ToString("yyy, MM, dd");
+            Console.WriteLine($"Today is {todaysDate}");
+            //Varible Containing Current Yeaar
+            int currentYear = DateTime.Today.Year;
+            DateTime today = DateTime.Today;
+            DateTime christmas = new DateTime(currentYear, 12, 25);
+            TimeSpan daysLeft = christmas - today;
+
+            int days = daysLeft.Days;
+            Console.WriteLine($"Therefore, there are {days.ToString()} days before Christmas, get your Christmas gifts ready!.");
+            Console.WriteLine("================================================================================================");
         }
     }
 }
