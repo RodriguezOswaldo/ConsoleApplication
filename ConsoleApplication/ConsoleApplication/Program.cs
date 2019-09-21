4932668c753c8fce99927f1031394328c7dd848e
Oswaldo Rodriguez <Owner2303@gmail.com>
2019-09-21 14:00:56 -0400

    Program from the book

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -23,6 +23,18 @@ namespace ConsoleApplication
             int days = daysLeft.Days;
             Console.WriteLine($"Therefore, there are {days.ToString()} days before Christmas, get your Christmas gifts ready!.");
             Console.WriteLine("================================================================================================");
+            Console.WriteLine("Program from the Book");
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
         }
     }
 }
